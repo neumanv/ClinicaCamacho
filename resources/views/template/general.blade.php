478715699027c99291a54cb654987c0483a154ca
neumanv <guillermoanv25@gmail.com>
2023-12-11 18:02:52 +0100

    Cambios

---
+++ resources/views/template/general.blade.php
@@ -0,0 +1,47 @@
+@extends('template.template')
+
+@section('header')
+
+@endsection
+
+@section('footer')
+    <div class="container-fluid justify-content-center bg-success pt-5 pb-5 text-light">
+        <div class="row justify-content-around">
+            <div class="col-12">
+                <div class="row justify-content-center">
+                    <div class="col-md-2 col-12 font-italic align-items-center mt-md-3 mt-3">
+                        <h5><span><img src="" class="img-fluid mb-1 "></span><b class="text-white">GREEN <span
+                                    class="text-white"> TRAVEL</span></b></h5>
+                        <small class="copy-rights cursor-pointer">&#9400;2023 Ilerna Daw</small><br>
+                        <small>Copyright. All Rights Reserved.</small>
+                    </div>
+                    <div class="col-md-2 col-12 my-sm-0 mt-5">
+                        <ul class="list-unstyled">
+                            <li class="mt-md-1">sdfsdf</li>
+                            <li class="mt-md-1">sdfsdf</li>
+                            <li class="mt-md-1">sdfsdf</li>
+                            <li class="mt-md-1">sdfsdf</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-12 my-sm-0 mt-5">
+                        <ul class="list-unstyled">
+                            <li class="mt-md-3 mt-4"></li>
+                            <li><a class="text-white" href="https://github.com/lauravcruz" target=_blank>Laura</a></li>
+                            <li><a class="text-white" href="https://github.com/neumanv" target=_blank>Guillermo</a></li>
+                            <li><a class="text-white" href="https://github.com/jgTrue" target=_blank>Jairo</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-12 my-sm-0 mt-5">
+                        <ul class="list-unstyled">
+                            <li class="mt-md-3 mt-4">¿Qúe necesitas?</li>
+                            <li>Licencia y seguridad</li>
+                            <li>Nuestra empresa</li>
+                            <li>Nuestros productos</li>
+                            <li>Precios</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clínica Camacho</title>
+    @vite(['resources/css/app.scss', 'resources/js/app.js', 'resources/css/custom.css'])
+    <script defer src="{{ URL::asset('assets/js/custom.js') }}"></script>
+</head>
+<body>
+    @yield('footer')
+</body>
+</html>
